4ecb74de1290258fbfe20ec247353a851ef90690
maricarmen <igmaricarmenorozco7@gmail.com>
2025-10-21 22:58:25 -0600

    Agregando una vista  llamada about.html.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    # Devuelve el título completo de la página
+  def full_title(page_title = '')
+    base_title = "Rails_App"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
